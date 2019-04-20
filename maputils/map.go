8c52946243b99efc4a6ad4fb237cc6ceb3b8f206
Minoru Osuka <minoru.osuka@gmail.com>
2019-04-20 20:42:39 +0900

    Update maputils

--- maputils/map.go
+++ maputils/map.go
@@ -15,201 +15,224 @@
 package maputils
 
 import (
-	"errors"
-	"fmt"
-	"regexp"
-	"strconv"
+	"encoding/json"
 	"strings"
 
-	"github.com/imdario/mergo"
+	"github.com/ghodss/yaml"
 	blasterrors "github.com/mosuka/blast/errors"
+	"github.com/stretchr/objx"
 )
 
-type NestedMap struct {
-	data interface{}
-	re   *regexp.Regexp
+type StructuredMap struct {
+	data objx.Map
 }
 
-func NewNestedMap() *NestedMap {
-	m := new(NestedMap)
-	m.data = make(map[string]interface{})
-	m.re = regexp.MustCompile("^(.+)\\[(\\d*)\\]$")
+func NewStructuredMap() *StructuredMap {
+	m := &StructuredMap{
+		data: objx.Map{},
+	}
+
 	return m
 }
 
-func (m *NestedMap) Get(path string) (interface{}, error) {
+func (m *StructuredMap) LoadJSON(data []byte) error {
+	var err error
+	m.data, err = objx.FromJSON(string(data))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *StructuredMap) LoadYAML(data []byte) error {
+	d, err := yaml.YAMLToJSON(data)
+
+	err = m.LoadJSON(d)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *StructuredMap) pathKeys(path string) []string {
+	keys := make([]string, 0)
+	for _, k := range strings.Split(path, "/") {
+		if k != "" {
+			keys = append(keys, k)
+		}
+	}
+
+	return keys
+}
+
+func (m *StructuredMap) makeSafePath(path string) string {
+	keys := m.pathKeys(path)
+
+	safePath := strings.Join(keys, "/")
+	//safePath = strings.Join(strings.Split(safePath, "."), "_")
+	safePath = strings.Join(strings.Split(safePath, "/"), objx.PathSeparator)
+
+	return safePath
+}
+
+func (m *StructuredMap) Set(path string, value interface{}) error {
+	m.data.Set(m.makeSafePath(path), value)
+
+	return nil
+}
+
+func (m *StructuredMap) Get(path string) (interface{}, error) {
+	value := m.data.Get(m.makeSafePath(path))
+	if value.IsBool() {
+		return value.Bool(), nil
+	} else if value.IsBoolSlice() {
+		return value.BoolSlice(), nil
+	} else if value.IsComplex64() {
+		return value.Complex64(), nil
+	} else if value.IsComplex64Slice() {
+		return value.Complex64Slice(), nil
+	} else if value.IsComplex128() {
+		return value.Complex128(), nil
+	} else if value.IsComplex128Slice() {
+		return value.Complex128Slice(), nil
+	} else if value.IsFloat32() {
+		return value.Float32(), nil
+	} else if value.IsFloat32Slice() {
+		return value.Float32Slice(), nil
+	} else if value.IsFloat64() {
+		return value.Float64(), nil
+	} else if value.IsFloat64Slice() {
+		return value.Float64Slice(), nil
+	} else if value.IsInt() {
+		return value.Int(), nil
+	} else if value.IsIntSlice() {
+		return value.IntSlice(), nil
+	} else if value.IsInt8() {
+		return value.Int8(), nil
+	} else if value.IsInt8Slice() {
+		return value.Int8Slice(), nil
+	} else if value.IsInt16() {
+		return value.Int16(), nil
+	} else if value.IsInt16Slice() {
+		return value.Int16Slice(), nil
+	} else if value.IsInt32() {
+		return value.Int32(), nil
+	} else if value.IsInt32Slice() {
+		return value.Int32Slice(), nil
+	} else if value.IsInt64() {
+		return value.Int64(), nil
+	} else if value.IsInt64Slice() {
+		return value.Int64Slice(), nil
+	} else if value.IsStr() {
+		return value.Str(), nil
+	} else if value.IsStrSlice() {
+		return value.StrSlice(), nil
+	} else if value.IsUint() {
+		return value.Uint(), nil
+	} else if value.IsUintSlice() {
+		return value.UintSlice(), nil
+	} else if value.IsUint8() {
+		return value.Uint8(), nil
+	} else if value.IsUint8Slice() {
+		return value.Uint8Slice(), nil
+	} else if value.IsUint16() {
+		return value.Uint16(), nil
+	} else if value.IsUint16Slice() {
+		return value.Uint16Slice(), nil
+	} else if value.IsUint32() {
+		return value.Uint32(), nil
+	} else if value.IsUint32Slice() {
+		return value.Uint32Slice(), nil
+	} else if value.IsUint64() {
+		return value.Uint64(), nil
+	} else if value.IsUint64Slice() {
+		return value.Uint64Slice(), nil
+	} else if value.IsUintptr() {
+		return value.Uintptr(), nil
+	} else if value.IsUintptrSlice() {
+		return value.UintptrSlice(), nil
+	} else if value.IsObjxMap() {
+		return value.ObjxMap(), nil
+	} else if value.IsObjxMapSlice() {
+		return value.ObjxMapSlice(), nil
+	} else if value.IsMSI() {
+		return value.MSI(), nil
+	} else if value.IsMSISlice() {
+		return value.MSISlice(), nil
+	} else if value.IsInter() {
+		return value.Inter(), nil
+	} else if value.IsInterSlice() {
+		return value.InterSlice(), nil
+	} else if value.IsNil() {
+		return nil, nil
+	} else {
+		return value, nil
+	}
+}
+
+func (m *StructuredMap) delete(keys []string, data interface{}) (interface{}, error) {
 	var err error
 
-	keys := make([]string, 0)
-	for _, k := range strings.Split(path, "/") {
-		if k != "" {
-			keys = append(keys, k)
-		}
-	}
+	if len(keys) >= 1 {
+		key := keys[0]
 
-	var value interface{}
-	value = m.data
-
-	for _, key := range keys {
-		if key == "" {
-			continue
-		}
-
-		i := -1
-		group := m.re.FindStringSubmatch(key)
-		if len(group) >= 3 {
-			key = group[1]
-			i, err = strconv.Atoi(group[2])
-			if err != nil {
-				return nil, err
-			}
-			if i < 0 {
-				return nil, errors.New(fmt.Sprintf("index out of bounds: %d", i))
-			}
-		}
-
-		switch value.(type) {
+		switch data.(type) {
 		case map[string]interface{}:
-			var exists bool
-			value, exists = value.(map[string]interface{})[key]
-			if !exists {
-				return nil, blasterrors.ErrNotFound
-			}
-		case *map[string]interface{}:
-			var exists bool
-			tmpValue := *value.(*map[string]interface{})
-			value, exists = tmpValue[key]
-			if !exists {
-				return nil, blasterrors.ErrNotFound
-			}
-		}
-
-		if i >= 0 {
-			arr := value.([]interface{})
-			if i >= len(arr) {
-				return nil, errors.New(fmt.Sprintf("index out of bounds: [index:%d] [array:%v]", i, arr))
-			}
-			value = arr[i]
-		}
-
-	}
-
-	return value, nil
-}
-
-func (m *NestedMap) makeMap(path string, value interface{}) map[string]interface{} {
-	keys := make([]string, 0)
-	for _, k := range strings.Split(path, "/") {
-		if k != "" {
-			keys = append(keys, k)
-		}
-	}
-
-	if len(keys) > 1 {
-		value = m.makeMap(strings.Join(keys[1:], "/"), value)
-	}
-
-	return map[string]interface{}{keys[0]: value}
-}
-
-func (m *NestedMap) Set(path string, value interface{}) error {
-	keys := make([]string, 0)
-	for _, k := range strings.Split(path, "/") {
-		if k != "" {
-			keys = append(keys, k)
-		}
-	}
-
-	var dst interface{}
-	dst = m.data
-
-	iter := newIterator(keys)
-	for {
-		key, err := iter.value()
-		if err != nil {
-			return err
-		}
-		if _, exists := dst.(map[string]interface{})[key]; !exists {
-			if !iter.hasNext() {
-				dst.(map[string]interface{})[key] = value
-				return nil
-			}
-
-			dst.(map[string]interface{})[key] = map[string]interface{}{}
-			dst = dst.(map[string]interface{})[key]
-		} else {
-			switch dst.(map[string]interface{})[key].(type) {
-			case map[string]interface{}:
-
-				switch value.(type) {
-				case map[string]interface{}:
-					err := mergo.Merge(&dst, value, mergo.WithOverride)
+			if _, exist := data.(map[string]interface{})[key]; exist {
+				// key exists
+				if len(keys) > 1 {
+					data.(map[string]interface{})[key], err = m.delete(keys[1:], data.(map[string]interface{})[key])
 					if err != nil {
-						return err
+						return nil, err
 					}
-				case []interface{}, interface{}:
-					dst.(map[string]interface{})[key] = value
+				} else {
+					//
+					mm := data.(map[string]interface{})
+					delete(mm, key)
+					data = mm
 				}
-
-			case []interface{}, interface{}:
-				dst.(map[string]interface{})[key] = value
+			} else {
+				// key does not exist
+				return nil, blasterrors.ErrNotFound
 			}
 		}
+	}
 
-		if iter.hasNext() {
-			iter.next()
-		} else {
-			break
-		}
+	return data, nil
+}
+
+func (m *StructuredMap) Delete(path string) error {
+	if path == "/" {
+		m.data = objx.Map{}
+		return nil
+	}
+
+	b, err := json.Marshal(&m.data)
+	if err != nil {
+		return err
+	}
+	var mm map[string]interface{}
+	err = json.Unmarshal(b, &mm)
+	if err != nil {
+		return err
+	}
+
+	data, err := m.delete(m.pathKeys(path), mm)
+	if err != nil {
+		return err
+	}
+
+	b, err = json.Marshal(&data)
+	if err != nil {
+		return err
+	}
+
+	m.data, err = objx.FromJSON(string(b))
+	if err != nil {
+		return err
 	}
 
 	return nil
-}
-
-func (m *NestedMap) Delete(path string) error {
-	keys := make([]string, 0)
-	for _, k := range strings.Split(path, "/") {
-		if k != "" {
-			keys = append(keys, k)
-		}
-	}
-
-	if len(keys) > 1 {
-		err := m.Delete(strings.Join(keys[1:], "/"))
-		if err != nil {
-			return err
-		}
-	}
-
-	delete(m.data.(map[string]interface{}), keys[0])
-
-	return nil
-}
-
-type iterator struct {
-	keys []string
-	pos  int
-}
-
-func newIterator(keys []string) *iterator {
-	return &iterator{
-		keys: keys,
-		pos:  0,
-	}
-}
-
-func (i *iterator) hasNext() bool {
-	return i.pos < len(i.keys)-1
-}
-
-func (i *iterator) next() bool {
-	i.pos++
-	return i.pos < len(i.keys)-1
-}
-
-func (i *iterator) value() (string, error) {
-	if i.pos > len(i.keys)-1 {
-		return "", errors.New("value is not valid after iterator finished")
-	}
-	return i.keys[i.pos], nil
 }
